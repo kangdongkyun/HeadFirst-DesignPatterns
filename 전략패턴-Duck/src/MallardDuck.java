0fde6b0e32dd610b8ba4253df284eedd15311992
kangdongkyun <35623439+kangdongkyun@users.noreply.github.com>
2018-08-30 16:02:42 +0900

    Add files via upload
    Study Strategy Pattern for JAVA - Duck

---
+++ 전략패턴-Duck/src/MallardDuck.java
@@ -0,0 +1,12 @@
+
+public class MallardDuck extends Duck {
+	public MallardDuck() {
+		quackBehavior = new Quack();
+		flyBehavior = new FlyWithWings();
+	}
+	@Override
+	public void display() {
+		System.out.println("저는 청둥오리입니다.");
+	}
+
+}
